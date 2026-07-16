0158746cc36266f978df83147f258218f5b941f5
ViniSchroeder <vinischroeder07@gmail.com>
2026-07-15 22:39:47 -0300

    Refresh cache for combat stability hotfix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sky-river-run-3d-4400';
-self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['./','./index.html','./css/styles.css?v=4400','./js/main.js?v=4400','./vendor/three.module.js','./vendor/three.core.js'])))});
+const CACHE='sky-river-run-3d-4402';
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['./','./index.html','./css/styles.css?v=4402','./js/bootstrap.js?v=4402','./js/main.js?v=4402','./vendor/three.module.js?v=4402','./vendor/three.core.js']))) });
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>caches.match(event.request))));
