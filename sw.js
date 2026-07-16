36ae3aaa2ce1e0c1d55f8d227ba401b509001d4a
ViniSchroeder <vinischroeder07@gmail.com>
2026-07-15 23:11:28 -0300

    Atualiza cache da versão 4.5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sky-river-run-3d-4402';
-self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['./','./index.html','./css/styles.css?v=4402','./js/bootstrap.js?v=4402','./js/main.js?v=4402','./vendor/three.module.js?v=4402','./vendor/three.core.js']))) });
+const CACHE='sky-river-run-3d-4500';
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['./','./index.html','./css/styles.css?v=4500','./js/bootstrap.js?v=4500','./js/main.js?v=4500','./vendor/three.module.js?v=4500','./vendor/three.core.js']))) });
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>caches.match(event.request))));
